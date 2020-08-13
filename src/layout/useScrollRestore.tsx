6edba1d1ae30d3d6d1414eab6991882826b1d45e
Brian <bdombro@gmail.com>
2020-08-13 16:15:05 -0500

    Upgrades to scroll restore

--- src/layout/useScrollRestore.tsx
+++ src/layout/useScrollRestore.tsx
@@ -10,64 +10,62 @@
 import React from "react";
 
 import { getKeyValueStore, KeyValueStore } from "../util/indexedDbKeyValueStore";
+import { waitFor } from "../util/wait";
 
 // @ts-ignore: Ignore when null b/c we never access storePromise when env = test
 const storePromise: Promise<KeyValueStore> =
   process.env.NODE_ENV !== "test" && getKeyValueStore("useScrollRestore", "historyIdToScrollUps", 48 * 60 * 60 * 1000);
 
-export function useScrollRestore(elementSelector: string) {
-  const recall = React.useCallback(() => {
-    const historyKey = window.history.state?.key ?? "entry"; // is null on first page view
-    const element = document.querySelector(elementSelector) as HTMLDivElement;
-    if (!element) {
-      console.warn("useScrollRestore.recall: Element not found");
-      return;
-    }
-    element.style.visibility = "hidden";
-    storePromise.then(async (store) => {
-      const scrollTop = await store.get<number>(historyKey);
-      const next = scrollTop ?? 0;
+const getElement = (selector: string) => document.querySelector(selector) as HTMLDivElement;
 
-      // Sometimes the page may not be fully loaded. If so, retry setting scroll position
-      // many times until success.
-      let success = false;
-      const set = () => {
-        if (!success) {
-          element.scrollTop = next;
-          if (element.scrollTop === next) {
-            success = true;
-            element.style.visibility = "visible";
-          }
+export function useScrollRestore(elementSelector: string) {
+  const recall = React.useCallback(async () => {
+    const historyKey = window.history.state?.key ?? "entry"; // is null on first page view
+    let element = getElement(elementSelector);
+    // If element not yet available, wait for it or bail
+    if (!element) element = await waitFor(() => getElement(elementSelector), { interval: 50, timeout: 2000 });
+    element.style.visibility = "hidden";
+    const store = await storePromise;
+    const next = (await store.get<number>(historyKey)) ?? 0;
+
+    // Sometimes the page may not be fully loaded. If so, retry setting scroll position
+    // many times until success.
+    let success = false;
+    const set = () => {
+      if (!success) {
+        element.scrollTop = next;
+        if (element.scrollTop === next) {
+          success = true;
+          element.style.visibility = "visible";
         }
-      };
-      const waitTime = 3000;
-      for (let i = 0; i < waitTime; i += 50) setTimeout(set, i);
-      setTimeout(() => {
-        element.style.visibility = "visible";
-      }, waitTime);
-    });
+      }
+    };
+    const waitTime = 3000;
+    for (let i = 0; i < waitTime; i += 50) setTimeout(set, i);
+    setTimeout(() => {
+      element.style.visibility = "visible";
+    }, waitTime);
   }, [elementSelector]);
-  const save = React.useCallback(() => {
+  const save = React.useCallback(async () => {
     const element = document.querySelector(elementSelector) as HTMLDivElement;
     if (!element) {
       console.warn("useScrollRestore.save: Element not found");
       return;
     }
     const historyKey = window.history.state?.key ?? "entry"; // is null on first page view
-    storePromise.then(async (store) => {
-      const scrollTopNow = element.scrollTop;
-      // If 0, don't save and remove stale saves here to reduce memory footprint
-      if (scrollTopNow === 0) {
-        const scrollTopLast = await store.get<number>(historyKey);
-        // if scrollTopLast, is stale and remove it
-        if (scrollTopLast !== undefined) {
-          await store.remove(historyKey);
-        }
-        // else no-op
-      } else {
-        await store.set(historyKey, element.scrollTop);
+    const store = await storePromise;
+    const scrollTopNow = element.scrollTop;
+    // If 0, don't save and remove stale saves here to reduce memory footprint
+    if (scrollTopNow === 0) {
+      const scrollTopLast = await store.get<number>(historyKey);
+      // if scrollTopLast, is stale and remove it
+      if (scrollTopLast !== undefined) {
+        await store.remove(historyKey);
       }
-    });
+      // else no-op
+    } else {
+      await store.set(historyKey, element.scrollTop);
+    }
   }, [elementSelector]);
 
   React.useEffect(() => {
--- src/util/wait.tsx
+++ src/util/wait.tsx
@@ -6,26 +6,51 @@
  * For example,
  * while(checkIfTrue()) await sleep(200);
  *
- */
+ * @param ms: Number of milliseconds to wait
+ *
+ **/
 export const wait = (ms: number) => {
   return new Promise(function (resolve) {
     setTimeout(resolve, ms);
   });
 };
 
-export const waitFor: waitFor = async (callback, options) => {
-  const { interval = 100, timeout = 10000, maxTries } = options;
+/**
+ * Return a promise that resolves when a callback resolves to truthy or on give-up (see options),
+ * and that promise resolves to the final result of the callback. Will throw an error on give-up
+ * by default.
+ *
+ * For example,
+ * const getElement = (selector: string) => document.querySelector(selector) as HTMLDivElement;
+ * const element = await waitFor(
+ *    () => getElement("#treasure"),
+ *    { interval: 50, timeout: 2000 }
+ * );
+ * element.innerHTML = "Harrrrr!"; // type = HTMLDivElement automagically!
+ * element.notAThing = "boo"; // fails type check!
+ *
+ * @param callback: A function to retry until truthy (promises work too)
+ * @param options: Options to control when and how to give-up
+ * @param options.interval: How often to retry. Will never retry until last is done, though.
+ * @param options.timeout: How long to wait before giving up
+ * @param options.maxTries: How many times to retry before giving up
+ * @param options.throwsErrorOnBail: Whether to throw an error or not on bail (aka give-up)
+ *
+ */
+export const waitFor: WaitFor = async (callback: any, options: any) => {
+  const { interval = 100, timeout = 10000, maxTries, throwsErrorOnBail = true } = options;
   const callbackPromise = async () => callback(); // promisify callback
   let tries = 0;
   const before = Date.now();
   const getElapsed = () => Date.now() - before;
-  let res = false;
+  let res;
 
   while (!res) {
     const timeBeforeCall = getElapsed();
 
     if (timeBeforeCall > timeout) {
-      throw new Error(`waitFor: timed out after ${timeout}ms`);
+      if (throwsErrorOnBail) throw new Error(`waitFor: timed out after ${timeout}ms`);
+      else return null as any;
     }
 
     res = await callbackPromise();
@@ -34,7 +59,8 @@ export const waitFor: waitFor = async (callback, options) => {
     tries++;
 
     if (maxTries && tries > maxTries) {
-      throw new Error(`waitFor: reached maxTries at ${tries}`);
+      if (throwsErrorOnBail) throw new Error(`waitFor: reached maxTries at ${tries}`);
+      else return null as any;
     }
 
     const timeUntilInterval = interval - callDuration;
@@ -44,11 +70,21 @@ export const waitFor: waitFor = async (callback, options) => {
       await wait(timeUntilInterval);
     }
   }
+  return res;
 };
-type waitFor = (
-  callback: () => boolean | Promise<boolean>,
-  options: { interval?: number; timeout?: number; maxTries?: number }
-) => Promise<void>;
+
+// Will return a promise of the same type that callback returns. Or you can specify the type
+// returned using syntax waitFor<string>(...). UNLESS, you specify throwsErrorOnBail = false;
+type WaitFor = {
+  <T extends any>(
+    callback: () => T,
+    options: { interval?: number; timeout?: number; maxTries?: number; throwsErrorOnBail?: true }
+  ): Promise<T>;
+  <T extends any>(
+    callback: () => T,
+    options: { interval?: number; timeout?: number; maxTries?: number; throwsErrorOnBail: false }
+  ): Promise<T | null>;
+};
 
 // const testWait = async () => {
 //   const before = Date.now();
